3ef94f71e4355de68d0ef719f02b54bcacc476de
SunLoveBing <457285506@qq.com>
2016-01-24 16:11:37 +0800

    Change some annotation
    Change some annotation into English

--- DataFileModify.m
+++ DataFileModify.m
@@ -5,7 +5,7 @@ clc;
 path = 'D:\Landmark Detection\Database\Test\SUV\无侧窗';
 dataPath = dir(path);
 
-%统计数据文件的个数
+% Count the number of folders which contain the data file. 
 dataFileNum = 0;
 for i = 1 : length(dataPath)
     if strcmp(dataPath(i).name,'.') || strcmp(dataPath(i).name,'..')
@@ -15,13 +15,13 @@ for i = 1 : length(dataPath)
     end
 end
 
-%获得每一个数据文件的路径
+% Get path of every data file.
 dataFilePath = cell(dataFileNum,1);
 for i = 1 : dataFileNum
     dataFilePath{i} = strcat(path,'\',dataPath(i+2).name);
 end
 
-%打开每一个数据文件,并进行数据整理
+% Open the data file and modify some data of this data file.
 for i = 1 : dataFileNum
     [fid,message] = fopen(dataFilePath{i},'r');
     if fid == -1
@@ -31,8 +31,7 @@ for i = 1 : dataFileNum
     end
     fclose(fid);
     
-    %数据整理
-    %第55条线
+    % Line of No.55
     data{1,1}{1085,1} = data{1,1}{1005,1};
     data{1,1}{1086,1} = data{1,1}{1006,1};
     data{1,1}{1090,1} = data{1,1}{1005,1};
@@ -42,7 +41,7 @@ for i = 1 : dataFileNum
     data{1,1}{1098,1} = data{1,1}{1005,1};
     data{1,1}{1099,1} = data{1,1}{1006,1};
 
-    %第56条线
+    % Line of No.56
     data{1,1}{1105,1} = data{1,1}{1005,1};
     data{1,1}{1106,1} = data{1,1}{1006,1};
     data{1,1}{1110,1} = data{1,1}{1010,1};
@@ -52,7 +51,7 @@ for i = 1 : dataFileNum
     data{1,1}{1118,1} = data{1,1}{1018,1};
     data{1,1}{1119,1} = data{1,1}{1019,1};
 
-    %第57条线
+    % Line of No.57
     data{1,1}{1125,1} = data{1,1}{1018,1};
     data{1,1}{1126,1} = data{1,1}{1019,1};
     data{1,1}{1130,1} = data{1,1}{1018,1};
@@ -62,12 +61,12 @@ for i = 1 : dataFileNum
     data{1,1}{1138,1} = data{1,1}{1018,1};
     data{1,1}{1139,1} = data{1,1}{1019,1};
     
-    %格式化输出到数据文件
+    % Output after Modification
     [fid,message] = fopen(dataFilePath{i},'w+');
     if fid == -1
         disp(message);
     else
-       %格式化输出
+       % Output with a certain format
         for j = 1 : 20 : 180
             fprintf(fid,'%s',data{1,1}{j,1},'  ',data{1,1}{j+1,1});
             fprintf(fid,'%8s',data{1,1}{j+2,1});
